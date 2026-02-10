6714084fc5738ff684a4d221bef59760cdbfa526
Bob <bob@flexus.team>
2026-02-10 11:57:10 +0000

    Bump version to 1.0.0 for production release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="roastmaster",
-    version="0.1.0",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         "flexus-client-kit @ git+https://github.com/smallcloudai/flexus-client-kit.git",
