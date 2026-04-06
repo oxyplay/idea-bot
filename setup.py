a4f8fb1f2c466a222c148f808a36718429df585a
oxyplay <max@oxyplay.com>
2026-04-06 13:44:38 +0100

    fix: align roastmaster version to 0.0.101

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="roastmaster",
-    version="0.0.8",
+    version="0.0.101",
     packages=find_packages(),
     install_requires=[
         "flexus-client-kit @ git+https://github.com/smallcloudai/flexus-client-kit.git",
