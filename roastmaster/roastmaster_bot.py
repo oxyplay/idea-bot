d4ae19282a71b7731d65200849e52f2b59c13436
oxyplay <max@oxyplay.com>
2026-04-07 13:05:29 +0100

    Remove ROASTMASTER_DEBUG_TOOLS debug functionality

--- roastmaster/roastmaster_bot.py
+++ roastmaster/roastmaster_bot.py
@@ -20,37 +20,6 @@ async def roastmaster_main_loop(fclient: ckit_client.FlexusClient, rcx: ckit_bot
     setup = ckit_bot_exec.official_setup_mixing_procedure(roastmaster_install.ROASTMASTER_SETUP_SCHEMA, rcx.persona.persona_setup)
     await ckit_integrations_db.main_loop_integrations_init(ROASTMASTER_INTEGRATIONS, rcx, setup)
 
-    if os.environ.get("ROASTMASTER_DEBUG_TOOLS") == "1":
-        @rcx.on_updated_message
-        async def _debug_log_message(msg) -> None:
-            content_preview = msg.ftm_content
-            if not isinstance(content_preview, str):
-                try:
-                    content_preview = json.dumps(content_preview)
-                except TypeError:
-                    content_preview = repr(content_preview)
-            if len(content_preview) > 400:
-                content_preview = content_preview[:400] + "..."
-
-            if msg.ftm_tool_calls:
-                logger.info(
-                    "%s debug assistant message alt=%s num=%s tool_calls=%s content=%r",
-                    rcx.persona.persona_id,
-                    msg.ftm_alt,
-                    msg.ftm_num,
-                    msg.ftm_tool_calls,
-                    content_preview,
-                )
-            elif msg.ftm_role in {"assistant", "tool"}:
-                logger.info(
-                    "%s debug message role=%s alt=%s num=%s content=%r",
-                    rcx.persona.persona_id,
-                    msg.ftm_role,
-                    msg.ftm_alt,
-                    msg.ftm_num,
-                    content_preview,
-                )
-
     try:
         while not ckit_shutdown.shutdown_event.is_set():
             await rcx.unpark_collected_events(sleep_if_no_work=10.0)
